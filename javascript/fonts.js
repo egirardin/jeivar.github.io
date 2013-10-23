af1471c7a57278e0abc0a92aef745569b5ca93ae
Jeivar <jfernadel@gmail.com>
2013-10-23 00:31:55 -0400

    adding

--- javascript/fonts.js
+++ javascript/fonts.js
@@ -1,1 +1,12 @@
-fonts.js
+WebFontConfig = {
+    google: { families: [ 'Roboto::latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })(); 
